007183d35e9ade38d5859580d617199a2a0bef43
xapstr <rajeshpr@xapstr.com>
2012-03-19 17:20:58 +0530

    entire

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+	<title>Resizable and autogrowing textarea</title>
+	<link rel="icon" href="../favicon.ico" type="image/x-icon" />
+	<script type="text/javascript" src="jquery-1.6.1.js"></script>
+	<script type="text/javascript" src="hycus-textarea.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+		    $('#txtarea').hycustextarea();
+		});
+	</script>
+
+	<style>
+		body{ margin:0px; }
+		#hycusdemosheader{ font-family:Georgia; font-size:30px;border-bottom:1px solid #676767;padding:5px 5px 8px;margin-bottom:10px; }
+		#idh, #idh:visited { color:#000; text-decoration:none; }
+		#hycusdemosheader span{ color:#999; }
+		#downloadbutton{ color:#ffffff;background:#03AEEB;float:right;font-size:20px;padding:5px;-moz-border-radius: 5px;-webkit-border-radius: 5px;border-radius: 5px;text-decoration:none; }
+		#downloadbutton:hover{ background:#888888; }
+		#hycucdemosbody{ padding:10px 0; min-height:500px; }
+		#hycusdemosfooter{ border-top:1px solid #676767; padding:10px; font-size:20px; }
+		#hycusdemosfooter a{ text-decoration:none; }
+		#wrapper{
+			width:600px;
+			margin:auto;
+		}
+		.spacer{
+			clear:both;
+			height:5px;
+		}
+
+		/*css for ajax tabs*/
+		div.grippie {
+			background:#EEEEEE url(grippie.png) no-repeat scroll center 2px;
+			border-color:#DDDDDD;
+			border-style:solid;
+			border-width:0pt 1px 1px;
+			cursor:s-resize;
+			height:9px;
+			overflow:hidden;
+		}
+		.resizable-textarea textarea {
+			display:block;
+			margin-bottom:0pt;
+			width:99%;
+			max-width:99%;
+			min-width:99%;
+			height: 20%;
+		}
+	</style>
+</head>
+<body>
+<div id="hycusdemosheader">Hycus Demos - <span>Resizable and autogrowing textarea</span>  <a href="http://www.box.net/shared/7z0e73vr68" id="downloadbutton" target="_blank">Download</a></div>
+<div id="hycucdemosbody">
+	<div id="wrapper">
+		<textarea name="txtarea" id="txtarea"></textarea>
+		</div>
+	</div>
+</div>
+<div id="hycusdemosfooter"><a href="http://hycus.com">&larr; Back to hycus.com</a> <span style="float:right;">&copy; hycus.com.</span></div>
+</body>
+</html>
